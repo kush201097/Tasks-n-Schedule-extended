9d8ea6760a8fe055d6b33aaf4d5dfc0925da45d1
Parthd9 <37976915+Parthd9@users.noreply.github.com>
2023-02-13 08:15:14 +0530

    Script modified for auto-increment columns.

--- db/release/tns_release_1.0.sql
+++ db/release/tns_release_1.0.sql
@@ -8,8 +8,9 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+DROP TABLE IF EXISTS attachment;
 CREATE TABLE [dbo].[attachment](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[taskid] [varchar](50) NOT NULL,
 	[filepath] [varchar](50) NOT NULL,
 	[userid] [int] NOT NULL,
@@ -23,8 +24,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS avtar;
 CREATE TABLE [dbo].[avtar](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[imagepath] [varchar](50) NULL,
 	[defaultpath] [varchar](50) NOT NULL,
 	[userid] [int] NOT NULL,
@@ -37,8 +39,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS comment;
 CREATE TABLE [dbo].[comment](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[userid] [int] NOT NULL,
 	[taskid] [int] NOT NULL,
 	[description] [varchar](256) NOT NULL,
@@ -50,8 +53,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS email;
 CREATE TABLE [dbo].[email](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[userid] [int] NOT NULL,
 	[useremail] [varchar](100) NOT NULL,
 	[htmlbody] [varchar](512) NOT NULL,
@@ -65,8 +69,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS license;
 CREATE TABLE [dbo].[license](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[orgid] [int] NOT NULL,
 	[licensekey] [varchar](50) NOT NULL,
 	[creationdate] [datetime] NOT NULL,
@@ -79,8 +84,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS module;
 CREATE TABLE [dbo].[module](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [varchar](50) NOT NULL,
 	[description] [varchar](100) NOT NULL,
 	[projectid] [int] NOT NULL,
@@ -91,8 +97,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS organization;
 CREATE TABLE [dbo].[organization](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[orgname] [varchar](50) NOT NULL,
 	[orgdomain] [varchar](50) NOT NULL,
 	[industrytype] [varchar](50) NULL,
@@ -110,8 +117,9 @@ GO
 ALTER TABLE [dbo].[organization] ADD  DEFAULT ((0)) FOR [parentorgid]
 GO
 
+DROP TABLE IF EXISTS project;
 CREATE TABLE [dbo].[project](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [varchar](50) NOT NULL,
 	[description] [varchar](100) NOT NULL,
 	[technologyid] [int] NOT NULL,
@@ -126,8 +134,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS sprint;
 CREATE TABLE [dbo].[sprint](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [varchar](50) NOT NULL,
 	[description] [varchar](100) NOT NULL,
 	[projectid] [int] NOT NULL,
@@ -149,8 +158,9 @@ GO
 ALTER TABLE [dbo].[sprint] ADD  DEFAULT ((0)) FOR [issprintreportsent]
 GO
 
+DROP TABLE IF EXISTS sprint_email_audit;
 CREATE TABLE [dbo].[sprint_email_audit](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[userid] [int] NOT NULL,
 	[useremail] [varchar](100) NOT NULL,
 	[sprintid] [int] NOT NULL,
@@ -167,8 +177,9 @@ GO
 ALTER TABLE [dbo].[sprint_email_audit] ADD  DEFAULT ((0)) FOR [sendby]
 GO
 
+DROP TABLE IF EXISTS task;
 CREATE TABLE [dbo].[task](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[title] [varchar](100) NOT NULL,
 	[description] [varchar](256) NOT NULL,
 	[typeid] [tinyint] NOT NULL,
@@ -188,8 +199,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS task_sprint;
 CREATE TABLE [dbo].[task_sprint](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[sprintid] [int] NOT NULL,
 	[taskid] [int] NOT NULL,
 PRIMARY KEY CLUSTERED 
@@ -199,8 +211,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS task_user;
 CREATE TABLE [dbo].[task_user](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[taskid] [int] NOT NULL,
 	[assignee] [int] NOT NULL,
 	[reporter] [int] NOT NULL,
@@ -215,8 +228,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS taskpriority;
 CREATE TABLE [dbo].[taskpriority](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [varchar](50) NOT NULL,
 	[description] [varchar](100) NOT NULL,
 PRIMARY KEY CLUSTERED 
@@ -226,8 +240,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS taskstatus;
 CREATE TABLE [dbo].[taskstatus](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [varchar](50) NOT NULL,
 	[description] [varchar](100) NOT NULL,
 PRIMARY KEY CLUSTERED 
@@ -237,8 +252,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS tasktype;
 CREATE TABLE [dbo].[tasktype](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [varchar](50) NOT NULL,
 	[description] [varchar](100) NOT NULL,
 PRIMARY KEY CLUSTERED 
@@ -248,8 +264,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS userroles;
 CREATE TABLE [dbo].[userroles](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[role] [varchar](50) NOT NULL,
 	[roledescription] [varchar](100) NULL,
 PRIMARY KEY CLUSTERED 
@@ -259,8 +276,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS users;
 CREATE TABLE [dbo].[users](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[firstname] [varchar](50) NOT NULL,
 	[lastname] [varchar](50) NOT NULL,
 	[email] [varchar](50) NOT NULL,
@@ -281,8 +299,9 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+DROP TABLE IF EXISTS version;
 CREATE TABLE [dbo].[version](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [varchar](50) NOT NULL,
 	[description] [varchar](100) NOT NULL,
 	[projectid] [int] NOT NULL,
